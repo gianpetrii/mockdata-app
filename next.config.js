2a1bd728feaa800b876aae1fb06fc1faf147ff38
Gianluca Petri <gianluca.petri@byma.com.ar>
2026-02-16 19:45:56 +0000

    Fix: Use serverExternalPackages for Next.js 16

--- next.config.js
+++ next.config.js
@@ -1,19 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      // Ignore optional knex dependencies we don't use
-      config.externals.push({
-        'better-sqlite3': 'commonjs better-sqlite3',
-        'mysql': 'commonjs mysql',
-        'oracledb': 'commonjs oracledb',
-        'pg-query-stream': 'commonjs pg-query-stream',
-        'sqlite3': 'commonjs sqlite3',
-        'tedious': 'commonjs tedious',
-      });
-    }
-    return config;
-  },
+  serverExternalPackages: [
+    'knex',
+    'pg',
+    'mysql2',
+    'better-sqlite3',
+    'sqlite3',
+    'oracledb',
+    'tedious',
+    'pg-query-stream',
+  ],
 };
 
 module.exports = nextConfig;
